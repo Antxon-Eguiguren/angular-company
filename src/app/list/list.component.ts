b58e9a63d3ccbe247bb5bd769a84792ac876fd2f
Antxon <antxonegi@gmail.com>
2020-02-12 12:29:59 +0100

    Filtrado por departamento terminado

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, DoCheck } from '@angular/core';
 import { Employee } from '../models/employee';
 
 @Component({
@@ -6,19 +6,27 @@ import { Employee } from '../models/employee';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, DoCheck {
 
-  @Input() employees: Employee;
+  @Input() employees: Employee[];
   @Output() sendDept: EventEmitter<string>;
+
+  deptSelected: string;
 
   constructor() {
     this.sendDept = new EventEmitter();
+    this.deptSelected = 'All';
   }
 
   ngOnInit() {
   }
 
+  ngDoCheck() {
+    // Probar como resetear el filtrado de empleados al crear un nuevo empleados para que se vean todos (Investigar ngDoCheck o ngOnChanges)
+  }
+
   handleChange($event) {
+    this.deptSelected = $event.target.value;
     this.sendDept.emit($event.target.value);
   }
 
