17205ce025e2434ae7e8cb421858abd49248f424
Antxon Eguiguren <antxonegi@gmail.com>
2020-02-14 22:41:16 +0100

    Implementación de la aplicación en el Local Storage, para guardar los datos en el navegador y que no se pierdan al recargar la página.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Employee } from './models/employee';
+import { parse } from 'querystring';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   arrEmployees: Employee[];
   deptSelected: string;
 
   constructor() {
-    this.arrEmployees = [
-      new Employee('Antxon', 'Eguiguren', 'antxon@gmail.com', 'Marketing'),
-      new Employee('Julia', 'Besada', 'julia@gmail.com', 'HR'),
-      new Employee('Aitziber', 'Redondo', 'aitziber@gmail.com', 'Development'),
-      new Employee('Jorge', 'Torroglosa', 'jorge@gmail.com', 'Design'),
-    ];
     this.deptSelected = 'All';
   }
 
+  ngOnInit() {
+    // Cuando arranco la aplicación, recupero del LS el array de empleados para poder trabajar con él. Si está vacío, lo inicializo a vacío para poder hacer push cuando creo el empleado (sino me da error). Si tiene datos, los recojo para empezar a trabajar con ellos.
+    if (JSON.parse(localStorage.getItem('array de empleados')) === null) {
+      this.arrEmployees = new Array();
+    } else {
+      this.arrEmployees = JSON.parse(localStorage.getItem('array de empleados'));
+    }
+  }
+
   handleEmployee($event) {
+    // Cada vez que se crea un nuevo empleado, lo meto en el array de empleados para poder trabajar con él y además lo guardo en LS para que al recargar la página ese empleado no se pierda.
     const employee = new Employee($event.name, $event.surname, $event.email, $event.department);
     this.arrEmployees.push(employee);
+    localStorage.setItem('array de empleados', JSON.stringify(this.arrEmployees));
   }
 
   handleSendDept($event) {
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -19,6 +19,7 @@ export class ListComponent implements OnInit {
   }
 
   ngOnInit() {
+
   }
 
   handleChange($event) {
@@ -33,6 +34,24 @@ export class ListComponent implements OnInit {
     if (position !== -1) {
       this.employees.splice(position, 1);
     }
+
+    // Borro el empleado del array del LS, para que al recargar la página, ese empleado no aparezca, ya que se ha borrado. Para ello:
+    // 1. Recupero todos los datos del LS.
+    const arrEmployeesLS = JSON.parse(localStorage.getItem('array de empleados'));
+
+    // 2. Busco el empleado y lo borro del array de empleados que he recuperado del LS.
+    const positionLS = arrEmployeesLS.findIndex(employeeLS => {
+      return employeeLS.name === $event.name;
+    });
+    if (positionLS !== -1) {
+      arrEmployeesLS.splice(positionLS, 1);
+    }
+
+    // 3. Borro todos los datos del LS.
+    localStorage.removeItem('array de empleados');
+
+    // 4. Cargo en el LS el array de empleados actualizado (sin el empleado a despedir).
+    localStorage.setItem('array de empleados', JSON.stringify(arrEmployeesLS));
   }
 
 }
